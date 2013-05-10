9a765a6bc5794a5dd30a10735d21fa36627c7c43
Arasthel <angel.arasthel@gmail.com>
2013-05-10 17:57:08 +0200

    Subidos archivos con la licencia en orden

--- src/org/arasthel/almeribus/utils/GPS.java
+++ src/org/arasthel/almeribus/utils/GPS.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Jorge Mart’n Espinosa (Arasthel).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Jorge Mart’n Espinosa (Arasthel) - initial API and implementation
+ ******************************************************************************/
 package org.arasthel.almeribus.utils;
 
 import android.content.Context;
